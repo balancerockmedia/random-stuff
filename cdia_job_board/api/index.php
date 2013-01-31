1679c0a94a3a35cbddbfa07802995cd97e1ffe4a
Daniel Johnson <dan@balancerockmedia.com>
2013-01-31 12:32:43 -0500

    edits

--- cdia_job_board/api/index.php
+++ cdia_job_board/api/index.php
@@ -9,7 +9,9 @@ require_once __DIR__.'/cdia/Database.php';
 
 // create the Silex application
 $app = new Silex\Application();
-$app['debug'] = TRUE;
+
+// debug mode
+$app['debug'] = FALSE;
 
 // inject Database object
 $app['db'] = new CDIA\Database();
