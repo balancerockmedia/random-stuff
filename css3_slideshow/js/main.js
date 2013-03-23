3b6ae06be1ad8f11798136bef995d5080f3a3426
Daniel Johnson <dan@balancerockmedia.com>
2013-03-23 10:02:12 -0400

    edits

--- css3_slideshow/js/main.js
+++ css3_slideshow/js/main.js
@@ -1,37 +1,37 @@
 var slideshow = function() {
-	$('#hero_nav').show();
-	
-	$('.hero').hide();
-	$('.hero:first').show();
-	
-	$('#hero_nav a').on('click', function(e) {
-		e.preventDefault();
-		
-		clearInterval(sequence);
-		
-		var id = $(e.target).attr('href');
-		
-		$('.hero').fadeOut();
-		$(id).fadeIn();
-	});
-	
-	var current = 0;
-	
-	var sequence = setInterval(function() {
-		if (current === $('.hero').length - 1) {
-			current = 0;
-		} else {
-			current++;
-		}
-		
-		$('.hero:visible').fadeOut();
-		$('.hero').eq(current).fadeIn();
-	}, 4000);
+    $('#hero_nav').show();
+    
+    $('.hero').hide();
+    $('.hero:first').show();
+    
+    $('#hero_nav a').on('click', function(e) {
+        e.preventDefault();
+        
+        clearInterval(sequence);
+        
+        var id = $(e.target).attr('href');
+        
+        $('.hero').fadeOut();
+        $(id).fadeIn();
+    });
+    
+    var current = 0;
+    
+    var sequence = setInterval(function() {
+        if (current === $('.hero').length - 1) {
+            current = 0;
+        } else {
+            current++;
+        }
+        
+        $('.hero:visible').fadeOut();
+        $('.hero').eq(current).fadeIn();
+    }, 4000);
 }
 
 
 $(function() {
-	if (!$('html').hasClass('cssanimations')) {
-		slideshow();
-	}
+    if (!$('html').hasClass('cssanimations')) {
+        slideshow();
+    }
 });
