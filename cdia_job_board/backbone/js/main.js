12c12cbb1a87e972651235fcabb8ec047a8c4819
Daniel Johnson <dan@balancerockmedia.com>
2013-04-06 18:11:17 -0400

    edits

--- cdia_job_board/backbone/js/main.js
+++ cdia_job_board/backbone/js/main.js
@@ -11,12 +11,10 @@ _.extend(CDIAJobBoard.events, Backbone.Events)
 
 // require.js config
 require.config({
-    urlArgs: 't=' + (new Date()).getTime()
+  urlArgs: 't=' + (new Date()).getTime()
 });
 
 // initialize app
 require(['app'], function(app) {
-  
   app.initialize();
-  
 });
