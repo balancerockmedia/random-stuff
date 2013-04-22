3241d8df1f0e88ec89d084abf74f05c8c1823e52
Daniel Johnson <dan@balancerockmedia.com>
2013-04-22 13:19:36 -0400

    edits

--- discount_rate/discount_rate.php
+++ discount_rate/discount_rate.php
@@ -14,55 +14,55 @@ setlocale(LC_MONETARY, 'en_US');
 function discount($products, $product, $discount, $all_products) {
     $new_prices = '';
     
-	if ($all_products === TRUE) {
-		foreach ($products as $key => $value) {
-			$orig_price = $value['price'];
-			$new_price = $orig_price - ($orig_price * ($discount * .01));
-			$new_prices .= '<p><strong>'.$key.'</strong>: '.money_format('%n', $new_price).'</p>';
-		}
-	} else {
-		foreach ($products as $key => $value) {
-			if ($product === $key) {
-				$orig_price = $value['price'];
-				$new_price = $orig_price - ($orig_price * ($discount * .01));
-				$new_prices .= '<p><strong>'.$key.'</strong>: '.money_format('%n', $new_price) .'</p>';
-				break;
-			}
-		}
-	}
-	
-	return $new_prices;
+    if ($all_products === TRUE) {
+        foreach ($products as $key => $value) {
+            $orig_price = $value['price'];
+            $new_price = $orig_price - ($orig_price * ($discount * .01));
+            $new_prices .= '<p><strong>'.$key.'</strong>: '.money_format('%n', $new_price).'</p>';
+        }
+    } else {
+        foreach ($products as $key => $value) {
+            if ($product === $key) {
+                $orig_price = $value['price'];
+                $new_price = $orig_price - ($orig_price * ($discount * .01));
+                $new_prices .= '<p><strong>'.$key.'</strong>: '.money_format('%n', $new_price) .'</p>';
+                break;
+            }
+        }
+    }
+    
+    return $new_prices;
 }
 
 // discount rate dropdown
 $rates = '';
 $count = 1;
 while ($count <= 99) {
-	$rates .= '<option value="'.$count.'">'.$count.'</option>';
-	$count++;
+    $rates .= '<option value="'.$count.'">'.$count.'</option>';
+    $count++;
 }
 
 // products
 $products = array(
-	'iPhone' => array('condition' => 'new', 'price' => '199.00'),
-	'MacBook' => array('condition' => 'used', 'price' => '525.00'),
-	'iMac' => array('condition' => 'new', 'price' => '1799.00')
+    'iPhone' => array('condition' => 'new', 'price' => '199.00'),
+    'MacBook' => array('condition' => 'used', 'price' => '525.00'),
+    'iMac' => array('condition' => 'new', 'price' => '1799.00')
 );
 
 // data for product dropdown
 $product_options = '';
 foreach($products as $key => $value) {
-	$product_options .= '<option value="'.$key.'">'.$key.': $'.$value['price'].'</option>';
+    $product_options .= '<option value="'.$key.'">'.$key.': $'.$value['price'].'</option>';
 }
 $product_options .= '<option value="all">All Products</option>';
 
 // decide what to do if it's one product or all products
 if (isset($_POST['submit'])) {
-	if ($_POST['product'] == 'all') {
-		$new_prices = discount($products, $_POST['product'], $_POST['rate'], TRUE);
-	} else {
-		$new_prices = discount($products, $_POST['product'], $_POST['rate'], FALSE);
-	}
+    if ($_POST['product'] == 'all') {
+        $new_prices = discount($products, $_POST['product'], $_POST['rate'], TRUE);
+    } else {
+        $new_prices = discount($products, $_POST['product'], $_POST['rate'], FALSE);
+    }
 }
 
 ?>
@@ -84,24 +84,24 @@ select { width: 150px; }
 <body>
 
 <div id="container">
-	
-	<form id="my_form" method="post" action="discount_rate.php"> 
-		<p><label for="product">Product:</label>
-		<select name="product" id="product">
-			<option value="null">Select One</option>
-			<?php echo $product_options; ?>
-		</select></p>
-		
-		<p><label for="rate">Discount Rate:</label>
-		<select name="rate" id="rate">
-			<option value="null">Select One</option>
-			<?php echo $rates; ?>
-		</select></p>
+    
+    <form id="my_form" method="post" action="discount_rate.php"> 
+        <p><label for="product">Product:</label>
+        <select name="product" id="product">
+            <option value="null">Select One</option>
+            <?php echo $product_options; ?>
+        </select></p>
+        
+        <p><label for="rate">Discount Rate:</label>
+        <select name="rate" id="rate">
+            <option value="null">Select One</option>
+            <?php echo $rates; ?>
+        </select></p>
 
-		<p><input type="submit" name="submit" value="Submit" /></p>
-	</form>
-	
-	<?php 
+        <p><input type="submit" name="submit" value="Submit" /></p>
+    </form>
+    
+    <?php 
     
     if (isset($new_prices)) { 
         echo $new_prices;
