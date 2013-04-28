52809d72dc48078c5748ead0077663af617d6b78
Daniel Johnson <dan@balancerockmedia.com>
2013-04-28 09:05:58 -0400

    edits

--- sports_league/application/controllers/team.php
+++ sports_league/application/controllers/team.php
@@ -9,12 +9,8 @@ class Team extends CI_Controller {
         $this->load->helper('date');
         
         $data['team'] = $this->Team_model->get_by_id($id);
-        
-        $team_id = $data['team']->id;
-        
-        $data['players'] = $this->Player_model->get_by_team($team_id);
-        
-        $data['games'] = $this->Team_model->get_schedule($team_id);
+        $data['players'] = $this->Player_model->get_by_team($data['team']->id);
+        $data['games'] = $this->Team_model->get_schedule($data['team']->id);
         
 		$this->load->view('team/view', $data);
 	}
