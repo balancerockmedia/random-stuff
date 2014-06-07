fd2ef0bac9494fa50ddd120e9c9574b0b5f70541
Dan Johnson <dan@balancerockmedia.com>
2014-06-07 10:06:23 -0400

    edits

--- paper-rock-scissors/src/Game.js
+++ paper-rock-scissors/src/Game.js
@@ -1,12 +1,11 @@
 var Game = function() {
-  
     this.choices = ['rock', 'paper', 'scissors'];
     this.your_score = 0;
     this.computer_score = 0;
 
     this.getRandomInt = function(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+    };
 
     this.takeTurn = function(you) {
         you = this.choices.indexOf(you);
@@ -21,33 +20,35 @@ var Game = function() {
                 'computer': computer
             };
         }
-    }
+    };
 
     this.compare = function(turn) {
-        switch (turn.you) {
-        case 0:
-            if (turn.computer === 1) {
-                return 'computer';
-            } else if (turn.computer === 2) {
-                return 'you';
-            }
-            break;
-
-        case 1:
-            if (turn.computer === 0) {
-                return 'you';
-            } else if (turn.computer === 2) {
-                return 'computer';
-            }
-            break;
-
-        case 2:
-            if (turn.computer === 0) {
-                return 'computer';
-            } else if (turn.computer === 1) {
-                return 'you';
-            }
-            break;
+        var winner;
+        
+        if (turn.you === 0 && turn.computer === 1) {
+            winner = 'computer';
         }
-    }
-}
+        
+        if (turn.you === 0 && turn.computer === 2) {
+            winner = 'you';
+        }
+        
+        if (turn.you === 1 && turn.computer === 0) {
+            winner = 'you';
+        }
+        
+        if (turn.you === 1 && turn.computer === 2) {
+            winner = 'computer';
+        }
+        
+        if (turn.you === 2 && turn.computer === 0) {
+            winner = 'computer';
+        }
+        
+        if (turn.you === 2 && turn.computer === 1) {
+            winner = 'you';
+        }
+        
+        return winner;
+    };
+};
