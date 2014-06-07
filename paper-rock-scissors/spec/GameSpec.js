7c9490976dc03e2577da573ff8f1f1c494b44b97
Dan Johnson <dan@balancerockmedia.com>
2014-06-07 09:01:34 -0400

    edits

--- paper-rock-scissors/spec/GameSpec.js
+++ paper-rock-scissors/spec/GameSpec.js
@@ -4,6 +4,53 @@ describe('Game', function() {
     beforeEach(function() {
         game = new Game();
     });
+    
+    it('getRandomInt helper returns 0 if Math.random() returns .1', function() {
+        spyOn(Math, 'random').and.returnValue(.1);
+        
+        var randomInt = game.getRandomInt(0, 2);
+        
+        expect(randomInt).toEqual(0);
+    });
+    
+    it('getRandomInt helper returns 1 if Math.random() returns .4', function() {
+        spyOn(Math, 'random').and.returnValue(.4);
+        
+        var randomInt = game.getRandomInt(0, 2);
+        
+        expect(randomInt).toEqual(1);
+    });
+    
+    it('getRandomInt helper returns 2 if Math.random() returns .7', function() {
+        spyOn(Math, 'random').and.returnValue(.7);
+        
+        var randomInt = game.getRandomInt(0, 2);
+        
+        expect(randomInt).toEqual(2);
+    });
+    
+    it('should take a turn', function() {
+        spyOn(game, 'getRandomInt').and.returnValue(1);
+        
+        var turn = game.takeTurn('rock');
+        
+        var expected = {
+            'you': 0,
+            'computer': 1
+        }
+        
+        expect(turn).toEqual(expected);
+    });
+    
+    it('should repeat turn if both players choose the same value', function() {
+        spyOn(game, 'getRandomInt').and.returnValue(0);
+        
+        var takeTurnSpy = spyOn(game, 'takeTurn').and.callThrough();
+        
+        var turn = game.takeTurn('rock');
+        
+        expect(takeTurnSpy.calls.count()).toEqual(2);
+    });
               
     it('you should win if you have a rock and the computer has scissors', function() {
         var winner = game.compare({
@@ -36,6 +83,24 @@ describe('Game', function() {
         var winner = game.compare({
           'you': 0,
           'computer': 1
+        })
+        
+        expect(winner).toEqual('computer');
+    });
+    
+    it('you should win if you have scissors and the computer paper', function() {
+        var winner = game.compare({
+          'you': 2,
+          'computer': 1
+        })
+        
+        expect(winner).toEqual('you');
+    });
+    
+    it('computer should win if the computer has scissors and you have paper', function() {
+        var winner = game.compare({
+          'you': 1,
+          'computer': 2
         })
         
         expect(winner).toEqual('computer');
--- paper-rock-scissors/src/Game.js
+++ paper-rock-scissors/src/Game.js
@@ -1,55 +1,53 @@
 var Game = function() {
   
-  this.choices = ['rock', 'paper', 'scissors'];
-  this.your_score = 0;
-  this.computer_score = 0;
-  
-  this.getRandomInt = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  
-  this.takeTurn = function() {
-    var you = prompt('rock, paper or scissors?');
-    you = this.choices.indexOf(you);
+    this.choices = ['rock', 'paper', 'scissors'];
+    this.your_score = 0;
+    this.computer_score = 0;
 
-    var computer = this.getRandomInt(0, 2);
-
-    if (you === computer) {
-      return this.takeTurn();
-    } else {
-      return {
-        'you': you,
-        'computer': computer
-      };
+    this.getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
-  }
-  
-  this.compare = function(turn) {
-    switch (turn.you) {
-    case 0:
-      if (turn.computer === 1) {
-        return 'computer';
-      } else if (turn.computer === 2) {
-        return 'you';
-      }
-      break;
 
-    case 1:
-      if (turn.computer === 0) {
-        return 'you';
-      } else if (turn.computer === 2) {
-        return 'computer';
-      }
-      break;
+    this.takeTurn = function(you) {
+        you = this.choices.indexOf(you);
 
-    case 2:
-      if (turn.computer === 0) {
-        return 'computer';
-      } else if (turn.computer === 1) {
-        return 'you';
-      }
-      break;
+        var computer = this.getRandomInt(0, 2);
+
+        if (you === computer) {
+            return this.takeTurn();
+        } else {
+            return {
+                'you': you,
+                'computer': computer
+            };
+        }
     }
-  }
-  
+
+    this.compare = function(turn) {
+        switch (turn.you) {
+        case 0:
+            if (turn.computer === 1) {
+                return 'computer';
+            } else if (turn.computer === 2) {
+                return 'you';
+            }
+            break;
+
+        case 1:
+            if (turn.computer === 0) {
+                return 'you';
+            } else if (turn.computer === 2) {
+                return 'computer';
+            }
+            break;
+
+        case 2:
+            if (turn.computer === 0) {
+                return 'computer';
+            } else if (turn.computer === 1) {
+                return 'you';
+            }
+            break;
+        }
+    }
 }
